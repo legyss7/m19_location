b26f7994937bdc3d5d483653363b60b70edf4f90
legyss7 <legyss@mail.ru>
2024-04-18 08:02:19 +0600

    added build

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.comGoogleDaggerHiltAndroid) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.safeargs) apply false
+    alias(libs.plugins.googleAndroidLibrariesMapsplatformSecretsGradlePlugin) apply false
+
+}
+
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        classpath(libs.androidx.navigation.safe.args.gradle.plugin)
+    }
+}
